7b9bef1e36eef102c93a27ccb82344162896962e
Dhairya Shah <65452005+DhairyaShah-Programmer@users.noreply.github.com>
2021-01-09 21:41:54 +0530

    Create wiki.py

---
+++ wiki.py
@@ -0,0 +1,15 @@
+import pyttsx3 as tts
+import wikipedia as wiki
+
+# Search Logic
+searchOpt = str(input("What do you want to search: "))
+summary = wiki.summary(searchOpt, sentences=7)
+print( "\n" + summary)
+page = wiki.page(searchOpt)
+
+# TTS Logic
+engine = tts.init()
+engine.say(summary)
+engine.runAndWait()
+
+print("Goodbye! Have a good day")
